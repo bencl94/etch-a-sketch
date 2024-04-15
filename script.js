163e86a5c00837e60ecb2d5d8afc2b8dfd5ed7ec
Benjamin Clauß <90100610+bencl94@users.noreply.github.com>
2024-04-15 15:45:35 +0200

    Fix for-loop condition

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ for (let i = 0; i < 16; i++){
     let lineDiv = document.createElement("div");
 
     lineDiv.classList.add("line");
-
-    for (let j = 0; j > 16; j++) {
+    
+    for (let j = 0; j < 16; j++) {
         let gridCell = document.createElement("div");
         gridCell.classList.add("cell");
-
+        
         lineDiv.appendChild(gridCell);
     }
-
+    
     containerDiv.appendChild(lineDiv);
 }
