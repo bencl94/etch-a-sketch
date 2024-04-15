4f1bed522cc247f370afa7e6dd4e6f3415e42da8
Benjamin Clauß <90100610+bencl94@users.noreply.github.com>
2024-04-15 16:23:28 +0200

    Add drawing feature

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const addStyleOnHover = (event) => {
+    event.target.classList.add("colored");
+}
 const containerDiv = document.querySelector(".container");
 
 for (let i = 0; i < 16; i++){
@@ -8,6 +11,8 @@ for (let i = 0; i < 16; i++){
     for (let j = 0; j < 16; j++) {
         let gridCell = document.createElement("div");
         gridCell.classList.add("cell");
+
+        gridCell.addEventListener("mouseenter", addStyleOnHover);
         
         lineDiv.appendChild(gridCell);
     }
