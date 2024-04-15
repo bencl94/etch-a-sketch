f2d7e74fb8cee8393b2038c865ee17f54155155b
Benjamin Clauß <90100610+bencl94@users.noreply.github.com>
2024-04-15 15:34:21 +0200

    Implement JS to add lines + gridCells

---
+++ script.js
@@ -0,0 +1,16 @@
+const containerDiv = document.querySelector(".container");
+
+for (let i = 0; i < 16; i++){
+    let lineDiv = document.createElement("div");
+
+    lineDiv.classList.add("line");
+
+    for (let j = 0; j > 16; j++) {
+        let gridCell = document.createElement("div");
+        gridCell.classList.add("cell");
+
+        lineDiv.appendChild(gridCell);
+    }
+
+    containerDiv.appendChild(lineDiv);
+}
